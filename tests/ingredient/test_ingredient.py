c5d4e081acdaa093948fb71804db70e0768c4cdd
Lucas <lm.spizzirri@gmail.com>
2023-11-01 16:11:49 -0300

    Req 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,26 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Restriction
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient1 = Ingredient("tomate")
+    ingredient2 = Ingredient("tomate")
+    assert hash(ingredient1) == hash(ingredient2)
+
+    ingredient3 = Ingredient("queijo mussarela")
+    assert hash(ingredient1) != hash(ingredient3)
+
+    assert ingredient1 == ingredient2
+
+    assert ingredient1 != ingredient3
+
+    assert repr(ingredient1) == "Ingredient('tomate')"
+
+    assert ingredient1.name == "tomate"
+
+    assert ingredient3.restrictions == {
+        Restriction.LACTOSE, Restriction.ANIMAL_DERIVED
+    }
+
+    assert hash(ingredient1) == hash(Ingredient("tomate"))
