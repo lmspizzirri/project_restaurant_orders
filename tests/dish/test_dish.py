2cf3d2f6ed5baef02cdb6d0b7aba7bef1a510dfb
Lucas <lm.spizzirri@gmail.com>
2023-11-03 15:36:37 -0300

    Req 2 fix

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -5,25 +5,25 @@ import pytest
 
 # Req 2
 def test_dish():
-    dish1 = Dish('Lasagna', 15.99)
-    dish2 = Dish('Pasta', 12.99)
+    dish1 = Dish('Batatas', 4.5)
+    dish2 = Dish('Ovos', 2.5)
 
-    assert dish1.name == 'Lasagna'
+    assert dish1.name == 'Batatas'
 
     with pytest.raises(TypeError, match="Dish price must be float."):
-        Dish("Lasagna", '1599')
+        Dish("Batatas", '450')
 
     with pytest.raises(ValueError, match="Dish price must be greater then zero."):
-        Dish('Pasta', -1) 
+        Dish('Ovos', -1) 
 
-    assert repr(dish1) == "Dish('Lasagna', R$15.99)"
+    assert repr(dish1) == "Dish('Batatas', R$4.50)"
     assert dish1 == dish1
     assert dish1 != dish2
 
     assert hash(dish1) == hash(repr(dish1))
     assert hash(dish1) != hash(repr(dish2))
 
-    dish1.add_ingredient_dependency(Ingredient('queijo mussarela'), 2)
-    assert dish1.recipe == {Ingredient ('queijo mussarela'): 2}
+    dish1.add_ingredient_dependency(Ingredient('Salt'), 100)
+    assert dish1.recipe == {Ingredient ('Salt'): 100}
     assert dish1.get_ingredients() == set(dish1.recipe.keys())
     assert dish1.get_restrictions() == set()
